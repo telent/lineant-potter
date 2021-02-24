13a8ca90a02fee011b409e4e9868af36fc9a54c4
Daniel Barlow <dan@telent.net>
2021-02-24 21:39:30 +0000

    stop hardocding the current directory name

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+CWD=`pwd`
 docker run -it \
     -e "BRANCH_NAME=lineage-15.1" \
     -e "DEVICE_LIST=potter" \
@@ -6,12 +7,12 @@ docker run -it \
     -e "INCLUDE_PROPRIETARY=false" \
     -e "WITH_SU=true" \
     -e "CUSTOM_PACKAGES=GmsCore GsfProxy FakeStore MozillaNlpBackend NominatimNlpBackend com.google.android.maps.jar FDroid FDroidPrivilegedExtension ANTRadioService ANTPlusPluginsService ANTUSBService" \
-    -v "/$HOME/src/android/l4mg/lineage:/srv/src" \
-    -v "/$HOME/src/android/l4mg/zips:/srv/zips" \
-    -v "/$HOME/src/android/l4mg/logs:/srv/logs" \
-    -v "/$HOME/src/android/l4mg/cache:/srv/ccache" \
-    -v "/$HOME/src/android/l4mg/keys:/srv/keys" \
-    -v "/$HOME/src/android/l4mg/userscripts:/srv/userscripts" \
-    -v "/$HOME/src/android/l4mg/manifests:/srv/local_manifests" \
+    -v "$CWD/lineage:/srv/src" \
+    -v "$CWD/zips:/srv/zips" \
+    -v "$CWD/logs:/srv/logs" \
+    -v "$CWD/cache:/srv/ccache" \
+    -v "$CWD/keys:/srv/keys" \
+    -v "$CWD/userscripts:/srv/userscripts" \
+    -v "$CWD/manifests:/srv/local_manifests" \
     -v "$HOME/src/android/telent:/srv/telent" \
     lineageos4microg/docker-lineage-cicd:latest
